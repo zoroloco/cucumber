e8b1d7d6f3cdf45089297c6ddaf412c4262fae5c
Kenneth Centurion <zoroloco@Kenneths-MBP.attlocal.net>
2023-03-28 22:21:19 -0500

    checking for api env config file first

--- api/src/config/config.service.ts
+++ api/src/config/config.service.ts
@@ -1,11 +1,18 @@
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
+import { Logger } from '@nestjs/common';
 
 export class ConfigService {
+  private readonly logger = new Logger(ConfigService.name);
   private readonly envConfig: { [key: string]: string };
 
   constructor(filePath: string) {
-	this.envConfig = dotenv.parse(fs.readFileSync(filePath));
+    if(fs.existsSync(filePath)){
+      this.envConfig = dotenv.parse(fs.readFileSync(filePath));
+    }else{
+      this.logger.warn('Did not find .env file. Using system envs instead');
+    }
+	
   }
 
   get(key: string): string {
