d407cb06f24e09ec9a920dd527420f444500c413
Kenneth Centurion <zoroloco@Kenneths-MacBook-Pro.local>
2023-03-30 16:02:45 -0500

    made resource server on spa point to https

--- spa/src/config.js
+++ spa/src/config.js
@@ -2,5 +2,5 @@
 export default {
   clientId: process.env.REACT_APP_CLIENTID,
   domain: process.env.REACT_APP_DOMAIN,
-  resourceServer: `http://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`
+  resourceServer: `https://${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`
 };
