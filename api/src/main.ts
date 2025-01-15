b09ed7c63c788177c95c4711454b8328eedafd7c
zoroloco <kencenturion@gmail.com>
2025-01-15 13:39:15 -0600

    added logging

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,9 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import {
-  WinstonModule
-} from 'nest-winston';
+import { WinstonModule } from 'nest-winston';
 import { AppConstants } from './app.constants';
 import { AppModule } from './app.module';
-import path = require('path');
-const fs = require('fs');
 
 async function bootstrap() {
   console.info('env:' + process.env.NODE_ENV);
@@ -44,7 +40,8 @@ async function bootstrap() {
   //}
 
   await app.listen(AppConstants.appPort);
-  Logger.log('API environment:' + process.env.NODE_ENV);
-  Logger.log('API version:' + process.env.API_VERSION);
+  Logger.log(`[API ENVIRONMENT: ${process.env.NODE_ENV}`);
+  Logger.log(`API VERSION: ${process.env.API_VERSION}`);
+  Logger.log(`DB USER: ${process.env.MYSQL_USER}`);
 }
 bootstrap();
