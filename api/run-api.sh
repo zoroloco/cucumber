71817a3469e1d747af0febf897994cf1f06e3074
zoroloco <kencenturion@gmail.com>
2025-01-15 15:11:44 -0600

    d

--- api/run-api.sh
+++ api/run-api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
-
-pm2 start.sh -i max --name "druidia-api" --env production --output "/opt/cucumber/api/logs/output.log" --error "/opt/cucumber/api/logs/error.log"
+export NODE_ENV=production
+pm2 start dist/main.js -i max --name "druidia-api" --env production --output "/opt/cucumber/api/logs/output.log" --error "/opt/cucumber/api/logs/error.log"
--- api/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ex
-
-npm run start:prod
