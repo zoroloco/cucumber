009e65da079fe790e4d4557a87a84cfd81ba63c3
zoroloco <kencenturion@gmail.com>
2025-01-15 14:51:56 -0600

    updated to fix env var

--- api/run-api.sh
+++ api/run-api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 
-pm2 start dist/main.js -i max --name "druidia-api" --env production --output "/opt/cucumber/api/logs/output.log" --error "/opt/cucumber/api/logs/error.log"
+pm2 npm start:prod -i max --name "druidia-api" --env production --output "/opt/cucumber/api/logs/output.log" --error "/opt/cucumber/api/logs/error.log"
